0a6e0b718f0d9eae43dd161c2088b86e1013aadf
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-11-19 13:41:08 -0700

    now you are logged in!

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { route } = require(".");
 const { User } = require("../../models");
 
 router.get("/", (req, res) => {
@@ -45,6 +46,32 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      // res.json({ user: dbUserData });
+  
+      // Verify user
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+  
+    });  
+  });
 
 /// PUT /api/users/1
 router.put("/:id", (req, res) => {
